7d1bd1b167a903d521bb5da31950c1b381254981
Jyoti2004 <jyotikalra004@gmail.com>
2024-10-15 14:19:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import gptlogo from './assets/chatgpt.svg';
 
+import addBtn from './assets/add-30.png';
+import messageicon from './assets/message.svg';
+import Home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+
+      <div className='sideBar'>
+        <div className='upperSide'>
+ 
+          <div className='upperSideTop'>
+            <img src={gptlogo} alt='Logo' className='logo'/>
+            <span className='brand'> Anish GPT</span>
+          </div>
+            <button className='midBtn'>
+              <img src={addBtn} alt='New Chat' className='addBtn'/>New Chat
+            </button>
+
+            <div className='upperSideBottom'>
+              <button className='query'>
+                <img src={messageicon} alt='' />What is the Programming Language!
+              </button>
+
+              <button className='query'>
+                <img src={messageicon} alt='' />How to use an API!
+              </button>
+            </div>
+
+        </div>
+        <div className='lowerSide'>
+          <div className="listitems"><img src={Home} alt="Home" className='listitemsImg'/>Home</div>
+          <div className="listitems"><img src={saved} alt="Saved" className='listitemsImg'/>Saved</div>
+          <div className="listitems"><img src={rocket} alt="Upgrade" className='listitemsImg'/>Upgarde to Pro</div>
+        </div>
+
+
+
+      </div>
+      <div className='main'>
+          <div className="chats"></div>
+
+          <div className="chatFooter">
+            <div className="inp">
+              <input type="text" placeholder='Send a Message' />
+            </div>
+          </div>
+      </div>
+
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+// ReactDOM is responsible for rendering React components into the DOM (Document Object Model). 
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// The App component is typically root component of your React application. It serves as the primary container for all other components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//createRoot--  Initializes a React root for rendering.-- is the new method to create a root for rendering React components. It replaces the older ReactDOM.render method and supports concurrent features, enabling better performance and user experience.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//Renders React elements into the DOM.
 root.render(
+  //React.StrictMode- tool for highlighting potential problems in application. It doesn't render any visible UI but activates checks and warnings for its child components.
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
